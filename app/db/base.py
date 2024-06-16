9b6f232ff45b4f84a5509dc77962a8feba139aaa
MIKU-7437 <nurshanov.dias@gmail.com>
2024-06-16 17:13:17 +0500

    second commit

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,7 @@
+from sqlalchemy.ext.declarative import declarative_base
 from app.models.users import User  # Import all models here
+
+# Create the Base instance
+Base = declarative_base()
 
 # Import additional models here as needed
